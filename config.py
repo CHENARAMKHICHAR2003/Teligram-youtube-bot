06b223b89509b551d08b2dba5b0b486b46e6e079
CHENARAMKHICHAR2003 <crchoudharyji1122@gmail.com>
2025-08-21 13:11:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-API_ID = 123456        # Your Telegram API ID (my.telegram.org से लो)
-API_HASH = "your_api_hash"
-BOT_TOKEN = "your_bot_token"
+API_ID = 27498866        # Your Telegram API ID (my.telegram.org से लो)
+API_HASH = "96fbb6ad2e11ab04e83ca09ef3f42455"
+BOT_TOKEN = "7242497051:AAEc9WhkZgahtMjUygbXtMK51t6s3wvAg3c"
